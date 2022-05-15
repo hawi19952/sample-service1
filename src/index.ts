95e3a845a2c5f97103fcd154569428edb1031701
hawi19952 <hussain@mohtwize.com>
2022-05-15 12:01:41 +0300

    complementing work

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ app.listen(port, () => {
 })
 
 app.use('/', (req, res) => {
-    res.send('Hello Gameel, I hope you are doing well, Wish you the best man!! ')
+    res.send('Hello Gameel, I hope you are doing well, Wish you the best man!!, awesome work dude')
 })
